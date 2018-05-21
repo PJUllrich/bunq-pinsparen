0463e903cb2eefbfc35b46cb2d3ea9fdd14d692b
Peter Ullrich <peter.j.ullrich@gmail.com>
2018-05-21 13:23:26 +0200

    Improved to which accounts notification filters should be added. Cleaned up config a bit.

--- src/api.py
+++ src/api.py
@@ -3,7 +3,7 @@ from bunq.sdk import context
 from bunq.sdk.context import ApiContext, ApiEnvironmentType, BunqContext
 from bunq.sdk.model.generated import endpoint
 from bunq.sdk.model.generated.endpoint import UserPerson
-from bunq.sdk.model.generated.object_ import NotificationFilter
+from bunq.sdk.model.generated.object_ import Certificate, NotificationFilter
 from os.path import isfile
 
 from src.config import Config
@@ -31,7 +31,7 @@ class API:
     def add_filters(self, callback_url: str, categories: list):
         # TODO: Merge old list with new list
         filters = [NotificationFilter('URL', callback_url, category) for category in categories]
-        for account in self._get_active_accounts():
+        for account in self._get_pinsparen_eligible_accounts():
             endpoint.MonetaryAccountBank.update(account.id_, notification_filters=filters)
 
     @staticmethod
@@ -45,6 +45,7 @@ class API:
                                          Config.get_option('DEVICE_DESCRIPTION'),
                                          [Config.get_option('PERMITTED_IP')])
         api_context.save(Config.get_option('API_CONTEXT_FILE_PATH'))
+        cls._pin_certificate()
 
     @staticmethod
     def _get_current_user():
@@ -57,11 +58,45 @@ class API:
         else:
             return ApiEnvironmentType.SANDBOX
 
-    @staticmethod
-    def _get_active_accounts():
+    @classmethod
+    def _get_pinsparen_eligible_accounts(cls):
         accounts_all = endpoint.MonetaryAccountBank.list().value
+        accounts_active = cls._filter_account_active(accounts_all)
+        accounts_included = cls._filter_account_excluded(accounts_active)
+        accounts_eligible = cls._filter_account_not_savings(accounts_included)
+
+        return accounts_eligible
+
+    @classmethod
+    def _filter_account_active(cls, accounts):
+        return list(filter(
+            lambda x: x.status == 'ACTIVE',
+            accounts
+        ))
+
+    @classmethod
+    def _filter_account_excluded(cls, accounts):
         excluded_accounts = Config.get_option('EXCLUDED_ACCOUNTS')
         return list(filter(
-            lambda x: x.status == 'ACTIVE' and x.alias[0].value not in excluded_accounts,
-            accounts_all
+            lambda x: x.alias[0].value not in excluded_accounts,
+            accounts
         ))
+
+    @classmethod
+    def _filter_account_not_savings(cls, accounts):
+        savings_account = Config.get_option('SAVINGS_IBAN')
+        return list(filter(
+            lambda x: x.alias[0].value != savings_account,
+            accounts
+        ))
+
+    @staticmethod
+    def _pin_certificate():
+        with open(Config.get_option('PEM_CERTIFICATE_PATH')) as pem:
+            certificates = endpoint.CertificatePinned.list().value
+            for cert in certificates:
+                endpoint.CertificatePinned.delete(cert.id_)
+
+            certificate = Certificate(pem.read())
+            response = endpoint.CertificatePinned.create([certificate])
+            print(response)
